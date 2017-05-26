3c4e6137be2d475de7112378071b33643e429bd7
katsew <y.katsew@gmail.com>
2017-05-27 01:30:53 +0900

    add sample codes

---
+++ example/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/katsew/kuji"
+	"github.com/katsew/kuji-redis"
+	"github.com/go-redis/redis"
+	"fmt"
+)
+
+func main () {
+	opt := redis.Options{
+		Addr: "127.0.0.1:6379",
+		Password: "",
+		DB: 0,
+	}
+	kr := kuji_redis.NewKujiRedisStrategy(&opt)
+	instance := kuji.NewKuji(kr)
+
+	c := []kuji.KujiCandidate{
+		kuji.KujiCandidate{
+			Id: 1,
+			Weight: 5,
+		},
+		kuji.KujiCandidate{
+			Id: 2,
+			Weight: 15,
+		},
+		kuji.KujiCandidate{
+			Id: 3,
+			Weight: 80,
+		},
+	}
+
+	_, err := instance.RegisterCandidatesWithKey("foo", c)
+	if (err != nil) {
+		panic(err)
+	}
+
+	PickOne(instance)
+
+}
+
+func PickOne(k kuji.Kuji) {
+	for i := 0; i < 1000; i++ {
+		num, err := k.PickOneByKey("foo")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Pickup string is", num)
+	}
+}
