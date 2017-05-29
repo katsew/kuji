412286c11557c0d5a018dd3d1a58fd64de60bb28
katsew <y.katsew@gmail.com>
2017-05-29 12:56:15 +0900

    add pick and delete

--- kuji.go
+++ kuji.go
@@ -14,6 +14,10 @@ func (k Kuji) PickOneByKey(s string) (string, error) {
 	return k.strategy.PickOneByKey(s)
 }
 
+func (k Kuji) PickAndDeleteOneByKey(s string) (string, error) {
+	return k.strategy.PickAndDeleteOneByKey(s)
+}
+
 func (k Kuji) RegisterCandidatesWithKey(s string, c []KujiCandidate) (int64, error) {
 	return k.strategy.RegisterCandidatesWithKey(s, c)
 }
--- strategy.go
+++ strategy.go
@@ -2,5 +2,6 @@ package kuji
 
 type KujiStrategy interface {
 	PickOneByKey(s string) (string, error)
+	PickAndDeleteOneByKey(s string) (string, error)
 	RegisterCandidatesWithKey(s string, c []KujiCandidate) (int64, error)
 }
