2b63cc425ddb58675aa54f5960e9076f43b0029a
katsew <y.katsew@gmail.com>
2017-05-30 12:43:06 +0900

    add util methods

--- util.go
+++ util.go
@@ -7,9 +7,14 @@ import (
 
 type Util struct {}
 
-func (u *Util) SpreadCandidates(c []KujiCandidate) []int64 {
+/**
+ * Spread all candidates into array
+ * Array length is equals to sum total weight of candidates.
+ * Values are Id for each candidates and spread in array equals to their weight value.
+ */
+func (u *Util) SpreadCandidates(candidates []KujiCandidate) []int64 {
 	var values []int64
-	for _, candidate := range c {
+	for _, candidate := range candidates {
 
 		for i := 0; i < int(candidate.Weight); i++ {
 			values = append(values, candidate.Id)
@@ -18,6 +23,38 @@ func (u *Util) SpreadCandidates(c []KujiCandidate) []int64 {
 	return values
 }
 
+/**
+ * Shuffle elements using rand.Perm
+ */
+func (u *Util) Shuffle(target []int64) []int64 {
+	n := len(target)
+	dest := make([]int64, n)
+	permutation := rand.Perm(n)
+	for i, v := range permutation {
+		dest[v] = target[i]
+	}
+	return dest
+}
+
+/**
+ * Use Fisher–Yates shuffle
+ * refs.
+ * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+ */
+func (u *Util) Shuffle2(target []int64) []int64 {
+	n := len(target)
+	dest := make([]int64, n)
+	copy(dest, target)
+	for i := n - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		dest[i], dest[j] = dest[j], dest[i]
+	}
+	return dest
+}
+
+/**
+ * Get random number with min - max range.
+ */
 func (u *Util) RandomNumberFromRange(min, max int64) int64 {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Int63n(max - min) + min
